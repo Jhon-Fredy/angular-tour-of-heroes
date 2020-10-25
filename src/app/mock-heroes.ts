8269bf11b461b1b190afc53bb54003a02bbcb40f
jhon <jhon_pb@@corhuila.edu.co>
2020-10-25 15:41:45 -0500

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { from } from 'rxjs';
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Jhon'},
+  { id: 2, name: 'Jhof'},
+  { id: 3, name: 'Kreken'},
+  { id: 4, name: 'Jhont'},
+  { id: 5, name: 'Krsr'},
+  { id: 6, name: 'Jhy'},
+  { id: 7, name: 'ont'}
+
+];
